397119a1cb77aea8ac9f9f36c9f3d1ef77035885
afaaqsaleem <afaaq501@gmail.com>
2023-12-25 19:51:13 +0100

    Created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
-function App(props) {
-  return (
-    <h1 className='text-red-500'>
-      App
-    </h1>
-  );
+export default function App (){
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/sign-in" element={<SignIn />}/>
+    <Route path="/sign-up" element={<SignUp />}/>
+    <Route path="/profile" element={<Profile />}/>
+    <Route path="/about" element={<About />}/>
+  </Routes>
+  </BrowserRouter>
 }
-
-export default App;
